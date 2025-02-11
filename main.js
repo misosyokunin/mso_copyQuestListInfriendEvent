91f2bd662d44a99d97792036fff0365a24457c9c
味噌味噌 <92548681+misosyokunin@users.noreply.github.com>
2025-02-11 09:55:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,63 @@
 javascript:(async()=>{
 "use strict";
 
+/*
+┏━━━━━━━━━━━━┓
+┃ＭＳＯ＿マケプレ自動入札┃
+┗━━━━━━━━━━━━┛
+Developer:
+	魚頭男（https://minesweeper.online/ja/player/16842796 ）
+Writing:
+	魚頭男（https://minesweeper.online/ja/player/16842796 ）
+
+贈れるクエストをいちいちコピペするのは面倒…。
+というわけで、ツールを作りました。
+
+=======================================================
+このツールはMinesweeper.Online様（https://minesweeper.online/ 、以下「ＭＳＯ」）より公認を受けていない、非公認のものです。
+当プログラムは、ＭＳＯ様とは一切関係ございませんので、このプログラムに関する質問・提言等の連絡は魚頭男（https://minesweeper.online/ja/player/16842796 、以下「魚」）までお願いします。
+当プログラムについて、ＭＳＯ様に連絡することは絶対にしないでください。
+運営者様並びにユーザー様にご迷惑にならないように努めておりますが、万が一のことがありましたら即削除いたします。
+=======================================================
+*/
+
+/*
+＝＝＝＝＝＝＝＝＝【使い方】＝＝＝＝＝＝＝＝＝
+このスクリプトを実行して身を任せるだけです。
+
+すべてのクエストを最短1秒で取得します
+（環境によっては2秒以上掛かるかもしれません）。
+スクリプト実行中は、できるだけタブの遷移やブラウザをバックグラウンドにしないようにしてください。
+
+なお、他言語でも同じようなことができると思います。
+ただ、このスクリプトのままでは動きませんので、適宜変えてください（「ja」や抽出文言）。
+
+*/
+
 /*＝＝＝＝＝＝＝＝＝＝【スクリプト実行確認】＝＝＝＝＝＝＝＝＝＝*/
+{
+	const TAR_URL = "https://minesweeper.online/ja/friend-quests";
+	const TAR_TITLE = "イベントクエスト画面";
+	if(location.href.includes(TAR_URL)){
+		
+	}else{
+		const result = window.confirm(`${TAR_TITLE}ではありません。\n${TAR_TITLE}へ飛びますか？\n（ページ遷移後に再度このスクリプトを実行してください。）`);
+		if(result){
+			location.href = TAR_URL;
+		}else{
+			alert(`${TAR_TITLE}（${TAR_URL}）を表示させてください。`);
+		}
+		return;
+	}
+}
 
-/*
-https://minesweeper.online/ja/arenas
-*/
 
+if(document.querySelector("#QuestsBlock > div").textContent.includes("贈れるクエスト")){
 
-/*＝＝＝＝＝＝＝＝＝＝【スクリプト実行前定数セット】＝＝＝＝＝＝＝＝＝＝*/
-
-const LIMIT_DAY = "01月 04日";	/*★取得する最後の日付（というよりも最初の日付）*/
-/*
-L8は
-"01月 05日"
-にする！
-*/
+}else{
+	alert("贈れるクエストがありません。");
+	return;
+}
 
 const Wait = {
 	waits : [],
@@ -39,436 +81,165 @@ const Wait = {
 };
 
 
-/*＝＝＝＝＝＝＝＝＝＝【実行前ユーザー入力】＝＝＝＝＝＝＝＝＝＝*/
-const SCRIPT_STYLE = document.createElement("style");
-SCRIPT_STYLE.innerHTML = `
-#___________bk{
-	position: fixed;
-	top: 0px;
-	left: 0px;
-	right: 0px;
-	margin: 0px auto;
-	background-color: rgba(0, 0, 0, 0.5);
-	width: 100vw;
-	font-size: 3ch;
-	z-index: 100;
-	display: flex;
-	height: 100vh;
-	padding: 1em;
-	justify-content: end;
+let Pagenation = document.querySelector("#QuestsBlock > div").nextElementSibling?.nextElementSibling;
+async function updatePageNation(callback){
+	Pagenation = document.querySelector("#QuestsBlock > div").nextElementSibling?.nextElementSibling;	/*再定義*/
+	const target = document.body;
+	const observer = new MutationObserver(async function (mutations) {
+		const tar = mutations[0].target;
+/*
+					console.log(tar);
+*/
+		if(tar.classList.contains("pagination")){
+			Wait.release();
+			Pagenation = document.querySelector("#QuestsBlock > div").nextElementSibling?.nextElementSibling;	/*再定義*/
+			observer.disconnect();
+		}
+	});
+	observer.observe(target, {
+		characterData: true,	/*テキストノードの変化を監視*/
+		childList: true,	/*子ノードの変化を監視*/
+		subtree: true,	/*子孫ノードも監視対象に含める*/
+	});
+	callback();	/*ページネーション操作を行う*/
+	await Wait.add();
 }
-#___________bk progress{
-	width: 100%;
-}
-#____StartDIV{
-	height: 100%;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-}
-#_____Poo{
-	display: none;
-}
-#____EndDIV{
-	height: 100%;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-}
-#____EndDIV > textarea{
-	height: 50%;
-	font-size: 16px;
-	field-sizing: content;
-}
-#____EndDIV > section{
-	height: 20%;
-	font-size: 16px;
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	grid-template-rows:
-	repeat(2, 1fr); gap: 0px;
-}
-.hiddenContent{
-	display: none !important;
-}
-`;
+let moveNextPage = function(){
+	isLooping = false;
+	return true;
+};
+if(Pagenation?.tagName === "UL"){
+	moveNextPage = (async () => {
+		const nextButton = Pagenation.querySelector(".next");
+		if(nextButton.classList.contains("disabled")){
+			isLooping = false;
+			return;
+		}else{
+			const callback = function(){
+				nextButton.click();
+			};
+			await updatePageNation(callback);
+		}
+	});
+	if(Pagenation.querySelector(".first").classList.contains("disabled")){
+	}else{
+		const callback = function(){
+			Pagenation.querySelector(".first").click();
+		};
+		await updatePageNation(callback);
+	}
+}else{
 
-const bk = document.createElement("div");
-bk.id = "___________bk";
-bk.append(SCRIPT_STYLE);
+}
+
+let pa = [];
+function getQuests(){
+	const trs = document.querySelectorAll("#QuestsBlock > table:first-of-type > tbody > tr");
+	trs.forEach((tr, index) => {
+		const td = tr.querySelectorAll("td");
+		const detail = (() => {
+			let rs = td[1].textContent;
+			rs = rs.replace("のカスタムを", "（☠💣%）");
+			rs = rs.replace(/(レベル|サイズ|NG|で|のゲームを|クリアする|を稼ぐ|以上|ゲームの報酬|を集める|\s|)/g, "");
+			return rs;
+		})();
+		const level = (() => {
+			let rs = td[0].textContent;
+			if(rs.includes("E")){
+			
+			}else{
+				rs += "_";
+			}
+			return rs;
+		})();
+		const ta = [];
+		ta.push(pa.length + 1);
+		ta.push(level);
+		ta.push(detail);
+	/*
+		ta.push(td[2].textContent);
+	*/
+		pa.push(`${ta.join("\t")}\n`);
+	});
+}
+
+let isLooping = true;
+while(isLooping){
+	getQuests();
+	console.log(pa);
+	await moveNextPage();
+	console.log(isLooping);
+};
+
+
+
+const wrapper = document.createElement("div");
+wrapper.style = "position: fixed; top: 0px; left: 0px; z-index: 999; right: 0px; bottom: 0px; margin: auto; padding: 5%; height: 90%; width: 90%; display: flex; flex-direction: column;";
+document.body.append(wrapper);
 {
-	const article = document.createElement("article");
-	article.id = "____StartDIV";
-	bk.append(article);
+	const textarea = document.createElement("textarea");
+	textarea.value = pa.join("");
+	textarea.style = "height: 80%;";
+	wrapper.append(textarea);
+	const footer = document.createElement("footer");
+	footer.style = "display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(2, 1fr); gap: 0px; height: 20%; padding: 0px;";
+	wrapper.append(footer);
 	{
-		const p = document.createElement("p");
-		p.textContent = "データ取得中…";
-		article.append(p);
+		const button = document.createElement("button");
+		button.type = "button";
+		button.textContent = "再採番";
+		button.addEventListener("click", () => {
+			const strs = textarea.value.split("\n");
+			const newstrs = strs.map((str, index) => {
+				return str.replace(/^\d+/, `${index + 1}`);
+			});
+			textarea.value = newstrs.join("\n");
+		});
+		footer.append(button);
 	}
 	{
 		const button = document.createElement("button");
 		button.type = "button";
-		button.textContent = "終了する";
+		button.textContent = "カスタム下見";
 		button.addEventListener("click", () => {
-			isLooping = false;
-		});
-		article.append(button);
-	}
-	{
-		const progress = document.createElement("progress");
-		progress.id = "____progress";
-		progress.max = 0;
-		progress.value = 0;
-		article.append(progress);
-	}
-}
-
-
-const poo = document.createElement("div");
-poo.id = "_____Poo";
-bk.append(poo);
-
-document.body.append(bk);
-
-/*＝＝＝＝＝＝＝＝＝＝【実行前定数セット】＝＝＝＝＝＝＝＝＝＝*/
-
-
-
-/*＝＝＝＝＝＝＝＝＝＝【実行中ユーザー入力】＝＝＝＝＝＝＝＝＝＝*/
-
-
-/*＝＝＝＝＝＝＝＝＝＝【データ取得】＝＝＝＝＝＝＝＝＝＝*/
-const MyDate = new class{
-	/*
-	const poo = [
-		"今日, 15:03:19",
-		"01月 16日, 02:25:24",
-		"2024年 01月 08日, 19:34:15",
-	];
-	poo.forEach((str) => {
-		const rs = MyDate.format(str);
-		const rn = MyDate.encode(rs);
-		const rss = MyDate.decode(rn);
-		const rsss = MyDate.getDateMinuteString(rss);
-		console.log(rs, rn, rss, rsss);
-	});
-	*/
-	/*
-	2025-01-29:15:03:19 1738130599000 Wed Jan 29 2025 15:03:19 GMT+0900 (日本標準時) 2025-01-29:15:03
-	2025-01-16:02:25:24 1736961924000 Thu Jan 16 2025 02:25:24 GMT+0900 (日本標準時) 2025-01-16:02:25
-	2024-01-08:19:34:15 1704710055000 Mon Jan 08 2024 19:34:15 GMT+0900 (日本標準時) 2024-01-08:19:34
-	*/
-	constructor(){
-		this.today = new Date();
-		this.year = this.today.getFullYear();
-		this.todayStr = this.getDateString(this.today);
-	}
-	encode(str){
-		return Date.parse(str);
-	}
-	decode(num){
-		return new Date(num);
-	}
-	format(str){
-		let rs = str;
-		rs = rs.replace("今日", this.todayStr);
-		if(rs.match(/^\d\d\d\d/)){
-		
-		}else{
-			rs = `${this.year}年${rs}`;
-		}
-		rs = rs.replace(/[\s|日]/g, "");
-		rs = rs.replace(/[年|月]/g, "-");
-		rs = rs.replace(/[,]/g, ":");
-		return rs;
-	}
-	getDateParts(date){
-		const ro = {
-			"YYYY": date.getFullYear().toString().padStart(4, '0'),
-			"MM": (date.getMonth() + 1).toString().padStart(2, '0'),
-			"DD": date.getDate().toString().padStart(2, '0'),
-			"HH": date.getHours().toString().padStart(2, '0'),
-			"mm": date.getMinutes().toString().padStart(2, '0'),
-		};
-	  return ro;
-	}
-	getDateString(date){
-		const rp = this.getDateParts(date);
-		return `${rp["YYYY"]}-${rp["MM"]}-${rp["DD"]}`;
-	}
-	getDateMinuteString(date){
-		const rp = this.getDateParts(date);
-		return `${rp["YYYY"]}-${rp["MM"]}-${rp["DD"]}:${rp["HH"]}:${rp["mm"]}`;
-	}
-};
-
-
-
-const ARENA_LIMIT_MINUTES = {
-	"L1": 15,
-	"L2": 20,
-	"L3": 30,
-	"L4": 40,
-	"L5": 50,
-	"L6": 60,
-	"L7": 75,
-	"L8": 90,
-};
-for(const [key, value] of Object.entries(ARENA_LIMIT_MINUTES)){
-	ARENA_LIMIT_MINUTES[`${key}E`] = value * 2;
-}
-for(const [key, value] of Object.entries(ARENA_LIMIT_MINUTES)){
-	ARENA_LIMIT_MINUTES[key] = `00:${value}:00`;
-}
-
-const putDatas = {};
-
-let isLooping = true;
-let progress;
-/*アリーナのリンクを取得する*/
-{
-	let isTwice = false;
-	const target = document.body;
-	const observer = new MutationObserver(async function (mutations) {
-		const tar = mutations[0].target;
-/*
-		console.log(tar);
-*/
-		if(tar.id === "stat_pagination"){	/*ページ遷移*/
-			if(!isTwice){	/*ページ遷移の瞬間とページ遷移後の2回ヒットする。遷移後だけに処理したい*/
-				isTwice = true;
+			const arr = [...textarea.value.matchAll(/\d+x\d+\/\d+/g)];
+			if(!arr){
+				alert("カスタムがありません。");
 				return;
 			}
-			isTwice = false;
-			Wait.release();
-		}
-	});
-	observer.observe(target, {
-		characterData: true,	/*テキストノードの変化を監視*/
-		childList: true,	/*子ノードの変化を監視*/
-		subtree: true,	/*子孫ノードも監視対象に含める*/
-	});
-	
-	/*1ページ目から始める*/
-	if(document.querySelector("#stat_pagination > .first")){
-		if(document.querySelector("#stat_pagination > .first").classList.contains("disabled")){
-			
-		}else{
-			isTwice = true;
-			document.querySelector("#stat_pagination > .first > a").click();
-			await Wait.add();
-		}
-	}
-	
-	let func = null;
-	let isLimit = false;
-	loop1: while(isLooping){
-		const trs = document.querySelectorAll("#stat_table_body > tr");
-		for(let i = 0; i < trs.length; i++){
-			const tr = trs[i];
-			const tds = tr.querySelectorAll("td");
-			if(tds[1].textContent === " "){	/*謎のスペース*/
-				break loop1;
-			}
-			if(tds[6].textContent.startsWith(LIMIT_DAY)){
-				if(isLimit){
-				
-				}else{
-					isLimit = true;
-					func = function(tds){
-						return !tds[6].textContent.startsWith(LIMIT_DAY);
-					};
-				}
-			}
-			if(func?.(tds)){
-				break loop1;
-			}
 /*
-			if(!tds[2].textContent.match(/アリーナ/)){
-				continue;
-			}
-*/
-			if(tds[3].querySelector("span").dataset.originalTitle === "進行中"){
-				continue;
-			}
-			
-			const link = tds[0].querySelector("a");
-			poo.append(link);
-			const to = {};
-			to["player"] = "あ";
-			to["mode"] = "あ";
-			to["startTime"] = MyDate.encode(MyDate.format(tds[6].textContent));
-			to["endTime"] = "あ";
-			to["theme"] = "0";	/*選択テーマ　"0":不明　"1":ランダム　"2":クラシック*/
-			to["progress"] = (() => {
-				if(tds[3].querySelector("span").dataset.originalTitle === "クリア"){
-					return "1";	/*成功*/
-				}
-				if(tds[3].querySelector("span").dataset.originalTitle === "エラー"){
-					return "4";	/*エラー*/
-				}
-				if(tds[5].textContent === "期限切れ"){
-					return "2";	/*失敗*/
-				}else{
-					return "3";	/*中断*/
-				}
-			})();
-			to["details"] = "あ";
-			putDatas[link.textContent] = to;
-			
-		}
-		await Wait.time(1);
-		const next = document.querySelector("#stat_pagination > .next");
-		if(!next){
-			break;
-		}
-		if(next.classList.contains("disabled")){
-			break;
-		}
-		next.click();
-		await Wait.add();
-	}
-	observer.disconnect();
-}
-
-
-{
-	/*アリーナリンクをクリックしていく*/
-	const target = document.body;
-	const observer = new MutationObserver(async function (mutations) {
-		const tar = mutations[0].target;
-	/*
-		console.log(tar);
-	*/
-		if(tar.id === "ArenaDetailBlock"){	/*アリーナ詳細表示*/
-			Wait.release();
-		}
-	});
-	observer.observe(target, {
-		characterData: true,	/*テキストノードの変化を監視*/
-		childList: true,	/*子ノードの変化を監視*/
-		subtree: true,	/*子孫ノードも監視対象に含める*/
-	});
-	
-	const links = poo.querySelectorAll("a");
-	progress = document.getElementById("____progress");
-	progress.max = links.length;
-	for(let i = 0; i < links.length; i++){
-		const link = links[i];
-		link.click();
-		await Wait.add();
-		const trs = document.querySelectorAll("#ArenaDetailBlock > .series-table > tbody > tr");
-		const to = putDatas[location.href.replace(/.+\//, "")];
-		to["player"] = trs[0].querySelector("a").href.replace(/^.+\//, "");
-		to["mode"] = trs[1].querySelector("td:nth-of-type(2) > span").textContent.replace(/^\s/, "");
-		to["details"] = trs[1].querySelector("td:nth-of-type(2) > div").textContent;
-/*
-		to["startTime"] = document.querySelectorAll("#stat_table_body > tr")[0].querySelectorAll("td")[6].textContent;
-*/
-		to["endTime"] = (()=>{
-			/*実質的な終了時間を求める。*/
-			/*
-				成功：開始時間＋経過時間（※ペナルティは含まないことに注意）
-				失敗：開始時間＋制限時間－ペナルティ
-				中断：開始時間＋経過時間
-				エラー：開始時間（※詳細がわからないhttps://minesweeper.online/ja/arena/1788901）
-			*/
-			let addTime = "00:00:00";
-			let subtractTime = "00:00:00";
-			if(to["progress"] === "1"){
-				addTime = trs[2].querySelector("td:nth-of-type(2) > span").textContent;
- 			}
-			if(to["progress"] === "2"){
-				addTime = ARENA_LIMIT_MINUTES[to["mode"].replace(/^.+\s/, "")];
-				if(trs[3].querySelector("td:nth-of-type(1)").textContent === "ペナルティ："){
-					subtractTime = trs[3].querySelector("td:nth-of-type(2) > span").textContent;
-				}
-			}
-			if(to["progress"] === "3"){
-				addTime = trs[2].querySelector("td:nth-of-type(2) > span").textContent;
-			}
-			if(to["progress"] === "4"){
-			}
-			
-			const endTime = MyDate.decode(to["startTime"]);
-			let [hh, mm, ss] = addTime.split(":");
-			endTime.setHours(endTime.getHours() + Number(hh));
-			endTime.setMinutes(endTime.getMinutes() + Number(mm));
-			endTime.setSeconds(endTime.getSeconds() + Number(ss));
-			[hh, mm, ss] = subtractTime.split(":");
-			endTime.setHours(endTime.getHours() - Number(hh));
-			endTime.setMinutes(endTime.getMinutes() - Number(mm));
-			endTime.setSeconds(endTime.getSeconds() - Number(ss));
-			return MyDate.encode(endTime);
-		})();
-/*
-		to["theme"] = "0";
-		to["progress"] = "成功";
-*/
-		progress.value++;
-		await Wait.time(1);
-	}
-	
-	observer.disconnect();
-}
-
-
-
-
-/*＝＝＝＝＝＝＝＝＝＝【データ表示】＝＝＝＝＝＝＝＝＝＝*/
-
-
-document.getElementById("____StartDIV").classList.add("hiddenContent");
-{
-	const article = document.createElement("article");
-	article.id = "____EndDIV";
-	{
-		const textarea = document.createElement("textarea");
-		textarea.value = JSON.stringify(putDatas, null, "\t");
-		article.append(textarea);
-		
-		const section = document.createElement("section");
-		article.append(section);
-		{
-			const button = document.createElement("button");
-			button.type = "button";
-			button.textContent = "📃コピーする";
-			button.addEventListener("click", () => {
-				textarea.select();
-				document.execCommand("copy");
-				window.getSelection?.().removeAllRanges();
-				textarea.blur();
-				button.textContent = "📃コピーしました！";
+			for(let i = 0; i < arr.length; i++){
+				const mat = arr[i];
 				setTimeout(() => {
-					button.textContent = "📃コピーする";
-				}, 3000);
-			});
-			button.style = "grid-column: span 2 / span 2;";
-			section.append(button);
-		}
-		{
-			const button = document.createElement("button");
-			button.type = "button";
-			button.textContent = "閉じる";
-			button.addEventListener("click", () => {
-				bk.remove();
-				location.href = "https://minesweeper.online/ja/arenas";
-			});
-			button.style = "grid-column-start: 1; grid-row-start: 2;";
-			section.append(button);
-		}
-		{
-			const button = document.createElement("button");
-			button.type = "button";
-			button.textContent = "";
-			button.style = "grid-column-start: 2; grid-row-start: 2;";
-			section.append(button);
-		}
+					window.open(`https://minesweeper.online/ja/start/${mat[0]}`, "_blank");
+				}, 1000);
+			}
+*/
+			const size = window.prompt("カスタムのサイズをコピペしてね\n例：100x100/2183", arr[0][0]);
+			if(size){
+				window.open(`https://minesweeper.online/ja/start/${size}`, "_blank")
+			}
+		});
+		footer.append(button);
 	}
-	bk.append(article);
+	{
+		const button = document.createElement("button");
+		button.type = "button";
+		button.textContent = "コピーしておわる";
+		button.style = "grid-column: span 2 / span 2;";
+		button.addEventListener("click", () => {
+			textarea.select();
+			document.execCommand("copy");
+	/*
+			window.getSelection?.().removeAllRanges();
+			textarea.blur();
+	*/
+			wrapper.remove();
+		});
+		footer.append(button);
+	}
 }
-
-
-/*＝＝＝＝＝＝＝＝＝＝【終了処理】＝＝＝＝＝＝＝＝＝＝*/
 
 })();
+
+
